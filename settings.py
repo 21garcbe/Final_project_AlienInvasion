96654d2748492f074af91abbb3dfc428edad7d0d
21garcbe <21garcbe@gmail.com>
2026-04-17 14:38:54 -0400

    made alien horizontal movement logic

--- settings.py
+++ settings.py
@@ -31,6 +31,6 @@ class Settings:
 
         #Alien settings
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
-        self.fleet_speed = 5
+        self.fleet_speed = 1
         self.alien_width = 40
         self.alien_height = 40
